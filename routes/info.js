d8f7164a431c2dcad9f8a924850e071972a33710
liangchengcheng <1038127753@qq.com>
2016-01-15 07:10:12 +0800

    node增删查
    node增删查

---
+++ routes/info.js
@@ -0,0 +1,71 @@
+/**
+ * Created by longlingxiu on 15/3/20.
+ */
+
+
+var mysql = require('mysql');
+
+
+
+/**
+ * 从数据库中获取信息
+ */
+function getDataFromDB( queryCallbak )
+{
+
+    var connection = mysql.createConnection({
+        host : '127.0.0.1',
+        user : 'root',
+        password : '',
+        port : '3306',
+        database : 'mydb'
+
+
+    });
+
+    connection.connect();
+
+
+    console.log('开始查询')
+
+    var querySql = 'select * from userinfos';
+
+    connection.query( querySql, function (err, result,fields ) {
+
+        if( err){
+            connection.close();
+            return;
+        }
+
+
+        // 将结果值传递过去
+        queryCallbak( result );
+
+    } );
+
+
+
+}
+
+
+module.exports = function( request, response, next ){
+
+    console.log('进入 info 路由回调');
+
+
+    // 获取数据库的查询结果
+    getDataFromDB( function( result ){
+
+
+        response.render('info', {
+            queryRlt : result
+        });
+
+    });
+
+
+
+};
+
+
+
